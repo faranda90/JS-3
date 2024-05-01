f987fc98f81f7d0faa3b74ff8d17d9df70621798
Facundo Aranda <facundor.aranda@gmail.com>
2024-05-01 00:25:37 -0300

    feo y casi funcional

--- codigo.js
+++ codigo.js
@@ -5,18 +5,17 @@ const botonVaciar = document.getElementById('vaciarBtn');
 
 
 
-
-function renderizarProductos(productos) {
-    for (const prod of productos) {
+function renderizarProductos(listaProds) {
+    for (const prod of listaProds) {
         contenedorProds.innerHTML += `
-        <div class="card col-md-3 bg-danger-subtle">
+    <div class="card col-md-3 bg-danger-subtle">
         <img class="card-img-top my-3" src=${prod.foto} alt=${prod.nombre}>
         <div class="card-body">
-          <h5 class="card-title">${prod.nombre}</h5>
-          <p class="card-text">Precio: $ ${prod.precio}</p>
-          <button class="btn btn-primary compra" id=${prod.id}>Comprar</button>
+        <h5 class="card-title">${prod.nombre}</h5>
+        <p class="card-text">Precio: $ ${prod.precio}</p>
+        <button class="btn btn-primary compra" id=${prod.id}>Comprar</button>
         </div>
-      </div>
+    </div>
         `
     }
 
@@ -26,7 +25,7 @@ function renderizarProductos(productos) {
         //opcion 1 - addEventListener()
         boton.addEventListener('click', () => {
             //buscar el objeto que tiene ese id
-            const prodACarrito = productos.find(prod => prod.id == boton.id);
+            const prodACarrito = listaProds.find(prod => prod.id == boton.id);
             console.log(prodACarrito);
             //cargar el producto al carrito de compras
             agregarACarrito(prodACarrito);
@@ -56,6 +55,84 @@ function agregarACarrito(producto) {
     </tr>
     `
     //agregar calculo de total
-    let totalAcumulado = carrito.reduce((acum, prod)=> acum + prod.precio,0);
-    document.getElementById('total').innerText = 'Total a pagar $: '+totalAcumulado;
+    let totalAcumulado = carrito.reduce((acum, prod) => acum + prod.precio, 0);
+    document.getElementById('total').innerText = 'Total a pagar $: ' + totalAcumulado;
+}
+
+//eventos de teclado
+const campoNombre = document.getElementById('nombre');
+const campoEmail = document.getElementById('email');
+
+//opcion 2
+campoNombre.onkeyup = () => {
+    if (campoNombre.value.length < 3) {
+        console.log('Nombre de menos de 3 letras 🚨');
+        campoNombre.style.color = 'red';
+    } else {
+        campoNombre.style.color = 'black';
     }
+}
+//opcion 2
+campoNombre.onchange = () => {
+    alert('Cambio el nombre en el formulario');
+    if (campoNombre.value == 'pepe') {
+        campoNombre.value = '';
+    }
+}
+
+/*CON OPCION 1: campoNombre.addEventListener('change', ()=>alert) */
+
+campoEmail.addEventListener('input', () => {
+    if ((!campoEmail.value.includes('@')) || (!campoEmail.value.includes('.'))) {
+        document.getElementById('mensaje').innerText = 'Ingrese un email valido !'
+    } else {
+        document.getElementById('mensaje').innerText = ''
+    }
+})
+
+
+//opcion 3 que viene del html linea 66
+function borrarCampos() {
+    campoNombre.value = '';
+    campoEmail.value = '';
+}
+
+
+//evento submit del formulario
+const formulario = document.getElementById('formulario');
+
+formulario.addEventListener('submit', validar);
+
+function validar(ev) {
+    if ((campoNombre.value == '') || (campoEmail.value == '')) {
+        ev.preventDefault();
+        alert('Ingrese nombre o email faltante 🚨');
+    }
+}
+
+//evento para finalizar la compra
+botonFinalizar.onclick = () => {
+    Toastify({
+        text: ahora.toLocaleString(DateTime.DATE_SHORT) + ' - Gracias por tu compra! Recibirás el pedido en 48hs',
+        duration: 5000,
+        gravity: 'bottom',
+        position: 'left',
+        close: true,
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+    }).showToast();
+
+    vaciarCarro();
+}
+
+function vaciarCarro() {
+    carrito = [];
+    tablaBody.innerHTML = '';
+    document.getElementById('total').innerText = 'Total a pagar $: ';
+}
+
+//vaciar carro
+botonVaciar.onclick = () => {
+    vaciarCarro();
+}
